f94db0764ba7b61fd1243bc92503625724a8cdb6
Andreas Hald <andreas.boris.hald@gmail.com>
2024-12-27 15:57:48 +0100

    simplify reproduction

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,19 @@
 <script lang="ts">
-    import Component from '$lib/component.svelte';
-    import Component1 from '$lib/component1.svelte';
+  import Component from "$lib/component.svelte";
+  import Component1 from "$lib/component1.svelte";
 </script>
 
 <h1>Bug reproduction</h1>
 
 <!-- This breaks on hydration -->
 <Component1>
-    <svelte:fragment slot="left">
-        <div class="mx-auto lg:mx-0">
-            <Component>
-                For a given location you get a condensed overview, you can use as an identifer of
-                red flags and easy benchmarking:
+  <svelte:fragment slot="slotA">
+    <Component>
+      some text without any element around it
 
-                <ul class="ml-10 mt-6 list-disc text-start">
-                    <li>Overall risk score</li>
-                    <li>Earthquake</li>
-                    <li>Storm</li>
-                    <li>Flood</li>
-                </ul>
-            </Component>
-        </div>
-    </svelte:fragment>
+      <ul>
+        <li>Overall risk score</li>
+      </ul>
+    </Component>
+  </svelte:fragment>
 </Component1>
